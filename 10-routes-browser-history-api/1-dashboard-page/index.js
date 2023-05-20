cbb1f27f7e6c802af869736d8ec9d7066057ac07
Эмиль Абдуллин <emilabdullin@MacBook-Pro-Emil.local>
2023-05-20 21:12:56 +0300

    feat(dashboard-page): Implement Dashboard Page

--- 10-routes-browser-history-api/1-dashboard-page/index.js
+++ 10-routes-browser-history-api/1-dashboard-page/index.js
@@ -8,5 +8,162 @@ import fetchJson from './utils/fetch-json.js';
 const BACKEND_URL = 'https://course-js.javascript.ru/';
 
 export default class Page {
+  element;
+  subElements = {};
+  components = {};
+  url = new URL('/api/dashboard/bestsellers', BACKEND_URL);
 
+  async updateComponents(from, to) {
+    const data = await this.loadData(from, to);
+
+    await this.components.sortableTable.update(data);
+
+    this.components.ordersChat.update(from, to);
+    this.components.salesChart.update(from, to);
+    this.components.customersChart.update(from, to);
+  }
+
+  loadData(from, to) {
+    this.url.searchParams.set('_start', '0');
+    this.url.searchParams.set('_end', '20');
+    this.url.searchParams.set('_sort', 'title');
+    this.url.searchParams.set('_order', 'asc');
+    this.url.searchParams.set('from', from.toISOString());
+    this.url.searchParams.set('to', to.toISOString());
+
+    return fetchJson(this.url);
+  }
+
+  initComponents() {
+    const now = new Date();
+    const to = new Date();
+    const from = new Date(now.setMonth(now.getMonth() - 1));
+
+    const rangePicker = new RangePicker({
+      from,
+      to,
+    });
+
+    const tableUrl = `api/dashboard/bestsellers?_start=0&_end=20&from=${from.toISOString()}&to=${to.toISOString()}`;
+
+    const sortableTable = new SortableTable(header, {
+      url: tableUrl,
+      isSortLocally: true,
+    });
+
+    const ordersChart = new ColumnChart({
+      url: 'api/dashboard/orders',
+      range: {
+        from,
+        to,
+      },
+      label: 'orders',
+      link: '#',
+    });
+
+    const salesChart = new ColumnChart({
+      url: 'api/dashboard/sales',
+      range: {
+        from,
+        to,
+      },
+      label: 'sales',
+    });
+
+    const customersChart = new ColumnChart({
+      url: 'api/dashboard/customers',
+      range: {
+        from,
+        to,
+      },
+      label: 'customers',
+    });
+
+    this.components = {
+      sortableTable,
+      ordersChart,
+      salesChart,
+      customersChart,
+      rangePicker,
+    };
+  }
+
+  renderComponents() {
+    Object.keys(this.components).forEach(componentName => {
+      const root = this.subElements[componentName];
+      const { element } = this.components[componentName];
+      root.append(element);
+    });
+  }
+
+  get template() {
+    return `
+      <div class="dashboard">
+        <div class="content__top-panel">
+          <h2 class="page-title">Dashboard</h2>
+          <div data-element="rangePicker"></div>
+        </div>
+        <div data-element="chartsRoot" class="dashboard__charts">
+          <div data-element="ordersChart" class="dashboard__chart_orders"></div>
+          <div data-element="salesChart" class="dashboard__chart_sales"></div>
+          <div data-element="customersChart" class="dashboard__chart_customers"></div>
+        </div>
+        <h3 class="block-title">Best sellers</h3>
+        <div data-element="sortableTable">
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+
+    this.element = element.firstElementChild;
+
+    this.subElements = this.getSubElements();
+
+    this.initComponents();
+    this.renderComponents();
+    this.initEventListeners();
+
+    return this.element;
+  }
+
+  getSubElements() {
+    const elements = this.element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((acc, subElement) => {
+      acc[subElement.dataset.element] = subElement;
+
+      return acc;
+    }, {});
+  }
+
+  initEventListeners() {
+    const { rangePicker } = this.components;
+
+    rangePicker.element.addEventListener('data-select', event => {
+      const { from, to } = event.detail;
+
+      this.updateComponents(from, to);
+    });
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+
+    this.subElements = null;
+    this.element = null;
+
+    for (const component of Object.values(this.components)) {
+      component.destroy();
+    }
+  }
 }
